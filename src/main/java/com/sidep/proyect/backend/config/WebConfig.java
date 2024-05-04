98fcde2613dd455d762f33276cda609b5af2a178
Fernando Arias <82820449+fernAP16@users.noreply.github.com>
2024-05-04 15:09:14 -0500

    Update WebConfig.java

--- src/main/java/com/sidep/proyect/backend/config/WebConfig.java
+++ src/main/java/com/sidep/proyect/backend/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://3.13.156.246:80", "http://3.13.156.246:443")
+                .allowedOrigins("http://localhost:3000", "http://3.13.156.246:80", "http://3.13.156.246:80", "http://3.13.156.246:443")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept")
                 .allowCredentials(true);
